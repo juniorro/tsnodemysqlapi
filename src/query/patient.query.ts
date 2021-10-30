7dd2cb08d3b5442f44e8ab0443550b46db92af2b
juniorro <juniorro98@outlook.com>
2021-10-29 22:55:17 -0400

    updated update query

--- src/query/patient.query.ts
+++ src/query/patient.query.ts
@@ -2,6 +2,6 @@ export const QUERY = {
     SELECT_PATIENTS: 'SELECT * FROM patients ORDER BY first_name LIMIT 50',
     SELECT_PATIENT: 'SELECT * FROM patients WHERE id = ?',
     CREATE_PATIENT: 'INSERT INTO patients SET ?',
-    UPDATE_PATIENT: 'UPDATE patients SET ? WHERE id = ?',
+    UPDATE_PATIENT: 'UPDATE patients SET first_name = ?, last_name = ?, email = ?, address = ?, diagnosis = ?, phone = ?, created_at = ?, image_url = ? WHERE id = ?',
     DELETE_PATIENT: 'DELETE FROM patients WHERE id = ?'
 };
