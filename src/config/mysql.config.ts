4b01109c09fdad6df0458e826879a686bba15589
juniorro <juniorro98@outlook.com>
2022-01-05 02:56:15 -0500

    update database name

--- src/config/mysql.config.ts
+++ src/config/mysql.config.ts
@@ -7,7 +7,7 @@ export const connection = async () => {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
     port: 3306 || process.env.DB_PORT,
     connectionLimit: 10 || process.env.DB_CONNECTION_LIMIT
   });
