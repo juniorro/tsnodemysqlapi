527af0929fec71e985776dddcec6781397094978
juniorro <juniorro98@outlook.com>
2022-01-05 02:50:01 -0500

    add try catch in controller

--- dbinit/init.sql
+++ dbinit/init.sql
@@ -20,6 +20,7 @@ CREATE TABLE patients
 ) AUTO_INCREMENT = 1;
 
 DELIMITER //
+CREATE PROCEDURE create_and_return(IN first_name VARCHAR(255), IN last_name VARCHAR(255), IN email VARCHAR(255), IN address VARCHAR(255), IN diagnosis VARCHAR(255), IN phone VARCHAR(255), IN image_url VARCHAR(255))
 BEGIN
 
     INSERT INTO patients(first_name, last_name, email, address, diagnosis, phone, image_url) VALUES (first_name, last_name, email, address, diagnosis, phone, image_url);
--- src/controller/patient.controller.ts
+++ src/controller/patient.controller.ts
@@ -10,53 +10,87 @@ import { QUERY } from '../query/patient.query';
 type ResultSet = [RowDataPacket[] | RowDataPacket[][] | OkPacket | OkPacket[] | ResultSetHeader, FieldPacket[]];
 
 export const getPatients = async (req: Request, res: Response): Promise<Response<Patient[]>> => {
-  const pool = await connection();
-  const result: ResultSet = await pool.query(QUERY.SELECT_PATIENTS);
-  return res.status(Code.OK)
-    .send(new HttpResponse(Code.OK, Status.OK, 'Patients retrieved', result[0]));
+  try {
+    const pool = await connection();
+    const result: ResultSet = await pool.query(QUERY.SELECT_PATIENTS);
+    return res.status(Code.OK)
+      .send(new HttpResponse(Code.OK, Status.OK, 'Patients retrieved', result[0]));
+  } catch (error: unknown) {
+    console.log(error);
+    return res.status(Code.INTERNAL_SERVER_ERROR)
+      .send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR, 'An error occurred'));
+  }
 };
 
 export const getPatient = async (req: Request, res: Response): Promise<Response<Patient[]>> => {
-  const pool = await connection();
-  const result: ResultSet = await pool.query(QUERY.SELECT_PATIENT, [req.params.patientId]);
-  if (((result[0]) as Array<any>).length > 0) {
-    return res.status(Code.OK)
-      .send(new HttpResponse(Code.OK, Status.OK, 'Patient retrieved', result[0]));
+  try {
+    const pool = await connection();
+    const result: ResultSet = await pool.query(QUERY.SELECT_PATIENT, [req.params.patientId]);
+    if (((result[0]) as Array<any>).length > 0) {
+      return res.status(Code.OK)
+        .send(new HttpResponse(Code.OK, Status.OK, 'Patient retrieved', result[0]));
+    } else {
+      return res.status(Code.NOT_FOUND)
+        .send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, 'Patient not found'));
+    }
+  } catch (error: unknown) {
+    console.log(error);
+    return res.status(Code.INTERNAL_SERVER_ERROR)
+      .send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR, 'An error occurred'));
   }
-  return res.status(Code.NOT_FOUND)
-    .send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, 'Patient not found'));
 };
 
 export const createPatient = async (req: Request, res: Response): Promise<Response<Patient>> => {
   let patient: Patient = { id: null, created_at: new Date(), ...req.body };
-  const pool = await connection();
-  const result: ResultSet = await pool.query(QUERY.CREATE_PATIENT, [patient]);
-  patient = { id: (result[0] as ResultSetHeader).insertId, ...req.body };
-  return res.status(Code.CREATED)
-    .send(new HttpResponse(Code.CREATED, Status.CREATED, 'Patient created', patient));
+  try {
+    const pool = await connection();
+    const result: ResultSet = await pool.query(QUERY.CREATE_PATIENT, [patient]);
+    patient = { id: (result[0] as ResultSetHeader).insertId, ...req.body };
+    return res.status(Code.CREATED)
+      .send(new HttpResponse(Code.CREATED, Status.CREATED, 'Patient created', patient));
+  } catch (error: unknown) {
+    console.log(error);
+    return res.status(Code.INTERNAL_SERVER_ERROR)
+      .send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR, 'An error occurred'));
+  }
 };
 
 export const updatePatient = async (req: Request, res: Response): Promise<Response<Patient>> => {
   let patient: Patient = req.body;
-  const pool = await connection();
-  const result: ResultSet = await pool.query(QUERY.SELECT_PATIENT, [req.params.patientId]);
-  if (((result[0]) as Array<any>).length > 0) {
-    const result: ResultSet = await pool.query(QUERY.UPDATE_PATIENT, [...Object.values(patient), req.params.patientId]);
-    return res.status(Code.OK)
-      .send(new HttpResponse(Code.OK, Status.OK, 'Patient updated', { ...patient, id: req.params.patientId }));
+  try {
+    const pool = await connection();
+    const result: ResultSet = await pool.query(QUERY.SELECT_PATIENT, [req.params.patientId]);
+    if (((result[0]) as Array<any>).length > 0) {
+      const result: ResultSet = await pool.query(QUERY.UPDATE_PATIENT, [...Object.values(patient), req.params.patientId]);
+      console.log(result);
+      return res.status(Code.OK)
+        .send(new HttpResponse(Code.OK, Status.OK, 'Patient updated', { ...patient, id: req.params.patientId }));
+    } else {
+      return res.status(Code.NOT_FOUND)
+        .send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, 'Patient not found'));
+    }
+  } catch (error: unknown) {
+    console.log(error);
+    return res.status(Code.INTERNAL_SERVER_ERROR)
+      .send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR, 'An error occurred'));
   }
-  return res.status(Code.NOT_FOUND)
-    .send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, 'Patient not found'));
 };
 
 export const deletePatient = async (req: Request, res: Response): Promise<Response<Patient>> => {
-  const pool = await connection();
-  const result: ResultSet = await pool.query(QUERY.SELECT_PATIENT, [req.params.patientId]);
-  if (((result[0]) as Array<any>).length > 0) {
-    const result: ResultSet = await pool.query(QUERY.DELETE_PATIENT, [req.params.patientId]);
-    return res.status(Code.OK)
-      .send(new HttpResponse(Code.OK, Status.OK, 'Patient deleted'));
+  try {
+    const pool = await connection();
+    const result: ResultSet = await pool.query(QUERY.SELECT_PATIENT, [req.params.patientId]);
+    if (((result[0]) as Array<any>).length > 0) {
+      const result: ResultSet = await pool.query(QUERY.DELETE_PATIENT, [req.params.patientId]);
+      return res.status(Code.OK)
+        .send(new HttpResponse(Code.OK, Status.OK, 'Patient deleted'));
+    } else {
+      return res.status(Code.NOT_FOUND)
+        .send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, 'Patient not found'));
+    }
+  } catch (error: unknown) {
+    console.log(error);
+    return res.status(Code.INTERNAL_SERVER_ERROR)
+      .send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR, 'An error occurred'));
   }
-  return res.status(Code.NOT_FOUND)
-    .send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, 'Patient not found'));
 };
